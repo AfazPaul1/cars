1b0de4d163aa36be1da119cfc6b812968524d275
AfazPaul1 <afazpaul@gmail.com>
2025-05-08 22:29:22 +0530

    made delete button click to dispatch a removecar action. some styling

--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -1,24 +1,36 @@
-import { Button, Grid, Typography, Box } from "@mui/material";
-import { useSelector } from "react-redux";
+import { Button, Grid, Typography, Box, Paper } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import { removeCar } from "../store";
 
 function CarList() {
+
+    const dispatch = useDispatch()
     const carsList = useSelector((state) => {
         return state.cars.carsList
     })
+    const handleClick = (id) => {
+        dispatch(removeCar(id))
+    }
     const renderedList = carsList.map((car) => {
         return (
+            <Paper sx={{p:1}} elevation={2}>
             <Grid sx={{margin:2}} key={car.id} container spacing={2} justifyContent="center">
-                <Grid  size={{xs: 8, sm:6, md:4, lg: 3}}>
-                    <Typography fullWidth variant="body1" gutterBottom>
-                        {car.name} - {car.cost}
-                    </Typography>
-                </Grid>
-                <Grid size={{xs: 2, sm:2, md:2, lg: 1}}>
-                    <Button fullWidth variant="contained"> 
-                        Delete
-                    </Button>
-                </Grid>
+                
+                    <Grid  size={{xs: 8, sm:6, md:4, lg: 3}}  alignItems="center">
+                        <Typography sx={{p:1}} fullWidth variant="body1" gutterBottom>
+                            {car.name} - {car.cost}
+                        </Typography>
+                    </Grid>
+                    <Grid size={{xs: 2, sm:2, md:2, lg: 1}}>
+                        <Button 
+                        onClick={() => handleClick(car.id) } 
+                        fullWidth variant="contained"> 
+                            Delete
+                        </Button>
+                    </Grid>
+                
             </Grid>
+            </Paper>
             )
         })
         console.log(carsList);
