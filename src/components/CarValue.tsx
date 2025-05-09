ef14dbbd9a54d0490f45eedd56f97b290f9ce788
AfazPaul1 <afazpaul@gmail.com>
2025-05-09 16:24:17 +0530

    implemented carvalue component.  since derived state we can implement the logic in the selector. the carvalue should reflect the filtered list so we have do the same filter here and reduce that carlist array using the reduce js method. it takes in 2 args, a function and a initial value. the function takes 2 args - the accumulator and the array item. inside to sum we do acc+item

--- src/components/CarValue.tsx
+++ src/components/CarValue.tsx
@@ -1,5 +1,22 @@
-function CarValue() {
-    return <div>CarValue</div>
+ import { Grid, TextField, Typography } from "@mui/material"
+import { useSelector } from "react-redux"
+function CarSearch() {
+    const sum = useSelector(({cars: {carsList, searchTerm}}) => 
+        carsList
+        .filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()))
+        .reduce((sum, car) => sum + car.cost, 0)
+    )
+
+    return (
+        <Grid container>
+            <Grid size={7}>
+                <div></div>
+            </Grid>
+            <Grid sx={{margin:1}} size={4}>
+                <Typography>Total Cost: ${sum}</Typography>
+            </Grid>
+        </Grid>
+    )
 }
 
-export default CarValue
+export default CarSearch
