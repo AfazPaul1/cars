49c047770bd05e9408d2253bd3be2dda00074adb
AfazPaul1 <afazpaul@gmail.com>
2025-05-09 18:50:30 +0530

    selected the name and carsList state using useSelector. used js boolean expressions to conditionally add the bold style to the mui typography. if name was falsy ie no one typed into name field the value of bold would be the first falsy value null hence fontweight value will be null. if name is true then we check whether theres a car in the list that matched typed name if yes then we return true. which we use to add classes to text conditionally. also used boolean expressions to conditionally render renderedList

--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -1,25 +1,30 @@
 import { Button, Grid, Typography, Box, Paper } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
 import { removeCar } from "../store";
+import '../style.css'
 
 function CarList() {
 
     const dispatch = useDispatch()
-    const carsList = useSelector(({cars: {carsList, searchTerm}}) => {
-        return carsList.filter((car) => {
-            return car.name.toLowerCase().includes(searchTerm.toLowerCase())
-          }) 
+    const {carsList, name} = useSelector(({form, cars: {carsList, searchTerm}}) => {
+        return {
+            carsList: carsList.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase())),
+            name: form.name
+        }
     })
     const handleClick = (id) => {
         dispatch(removeCar(id))
     }
     const renderedList = carsList.map((car) => {
+
+        const bold = name && car.name.toLowerCase().includes(name.toLowerCase())
+
         return (
             <Paper key={car.id} sx={{p:1}} elevation={2}>
             <Grid sx={{margin:2}}  container spacing={2} justifyContent="center">
                 
                     <Grid  size={{xs: 8, sm:6, md:4, lg: 3}}  alignItems="center">
-                        <Typography sx={{p:1}} fullWidth variant="body1" gutterBottom>
+                        <Typography sx={{p:1,  fontWeight: `${bold && 'bold'}`}} fullWidth variant="body1" gutterBottom>
                             {car.name} - {car.cost}
                         </Typography>
                     </Grid>
@@ -39,10 +44,9 @@ function CarList() {
         
     return (
         <div>
-            {carsList.length!==0? 
-            <Box component="section" sx={{ p: 2, margin:2 }}>
+            {renderedList && <Box component="section" sx={{ p: 2, margin:2 }}>
             {renderedList}
-            </Box> : <div></div>}
+            </Box>}
         </div>
         
     )
