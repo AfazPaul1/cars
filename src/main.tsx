c8eb1430113490424f9e3ff6e55cd5c34bd0d092
AfazPaul1 <afazpaul@gmail.com>
2025-05-28 14:41:47 +0530

    refactor(ts): used non null assertion operator body: - post fix expression operator "!" to assert that its operand is non null and non undefined in contexts where the type check is unable to confirm it

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import App from './App'
 import {store} from './store'
 import { Provider  } from "react-redux";
-const el = document.getElementById('root')
+import './style.css'
+const el = document.getElementById('root')!
 const root = createRoot(el)
 
 root.render(
